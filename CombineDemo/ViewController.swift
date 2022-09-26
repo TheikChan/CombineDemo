59d1ab662ae0c7b72403bc7c666f83fc7a00a2fb
Theik Chan <theik.chan@wavemoney.com.mm>
2022-09-26 13:35:58 +0630

    add message and combine publisher subscriber

--- CombineDemo/ViewController.swift
+++ CombineDemo/ViewController.swift
@@ -6,14 +6,51 @@
 //
 
 import UIKit
+import Combine
+
+extension Notification.Name {
+    static let newMessage = Notification.Name("newMessage")
+}
+
+struct Message {
+    let content: String
+    let author: String
+}
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var allowMessageSwitch: UISwitch!
+    @IBOutlet weak var btnSendMessage: UIButton!
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    @Published var canSendMessage: Bool = false
+    
+    private var switchSubscriber: AnyCancellable?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupProcessingChain()
+    }
+    
+    func setupProcessingChain() {
+        switchSubscriber = $canSendMessage.receive(on: DispatchQueue.main).assign(to: \.isEnabled, on: btnSendMessage)
+        let messagePublisher = NotificationCenter.Publisher(center: .default, name: .newMessage)
+            .map { notification -> String? in
+                return (notification.object as? Message)?.content ?? ""
+            }
+        let messageSubscriber = Subscribers.Assign(object: messageLabel, keyPath: \.text)
+        
+        messagePublisher.subscribe(messageSubscriber)
     }
 
-
+    @IBAction func didSwitch(_ sender: UISwitch) {
+        canSendMessage = sender.isOn
+    }
+    
+    @IBAction func sendMessage(_ sender: Any) {
+        let message = Message(content: "The current time is \(Date())", author: "Me")
+        NotificationCenter.default.post(name: .newMessage, object: message)
+    }
+    
 }
 
